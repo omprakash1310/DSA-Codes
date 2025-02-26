9ab695a2855b3e69eb25cd5e464d401a48cf3e02
omprakash1310 <142621908+omprakash1310@users.noreply.github.com>
2025-02-27 00:35:30 +0530

    Time: 2495 ms (14.03%), Space: 34.4 MB (85.13%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+    // Better Aproach
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+};
