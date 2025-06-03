7c4ced33da81535e27a3fd6a3c8dbdaa778aa58b
omprakash1310 <142621908+omprakash1310@users.noreply.github.com>
2025-06-04 00:02:12 +0530

    Time: 0 ms (100%), Space: 10.5 MB (56.44%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int countPartitions(vector<int> &a, int maxSum) {
-    int n = a.size(); //size of array.
-    int partitions = 1;
-    long long subarraySum = 0;
-    for (int i = 0; i < n; i++) {
-        if (subarraySum + a[i] <= maxSum) {
-            //insert element to current subarray
-            subarraySum += a[i];
-        }
-        else {
-            //insert element to next subarray
-            partitions++;
-            subarraySum = a[i];
+int countStudent(vector<int> &arr, int pages){
+    int student =1;
+    long long pagesStudent =0;
+    for(int i =0; i<arr.size(); i++){
+        if(pagesStudent + arr[i] <= pages){
+            pagesStudent += arr[i];
+        }else{
+            student += 1;
+            pagesStudent = arr[i];
         }
     }
-    return partitions;
+    return student;
 }
-    int splitArray(vector<int>& a, int k) {
-         int low = *max_element(a.begin(), a.end());
-    int high = accumulate(a.begin(), a.end(), 0);
-    //Apply binary search:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        int partitions = countPartitions(a, mid);
-        if (partitions > k) {
-            low = mid + 1;
-        }
-        else {
-            high = mid - 1;
-        }
+
+int findPages(vector<int> &arr, int n, int m){
+    if( m > n) return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while(low<= high){
+        int mid = (low + high) /2;
+        int student = countStudent(arr, mid);
+        if(student > m){
+            low = mid +1;
+        }else high = mid-1;
     }
     return low;
+}
+    int splitArray(vector<int>& nums, int k) {
+        return findPages(nums, nums.size(), k);
     }
 };
